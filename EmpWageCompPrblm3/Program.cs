fa9e5481b9a545f81a3cd61a1bcde77057e3c63e
naveendowd <119478972+naveendowd@users.noreply.github.com>
2023-06-23 16:09:29 +0530

    Welcome message

---
+++ EmpWageCompPrblm3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageCompPrblm3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome to Emp Wage Comp Prblm -4");
+            }
+        }
+    }
+}
